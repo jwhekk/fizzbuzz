b31b1e222ae68b8ae10d932f6ec6ee926e9ee3bb
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-14 18:06:59 +0100

    passed second test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(number)
+  if number % 15 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
   'fizz'
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe "fizzbuzz" do
   it "returns fizz when passed 3" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it "returns fizzbuzz when number is divisible by 15" do
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
+  end
+
 end
